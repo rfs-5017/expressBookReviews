343fd78a23607809a05c7b648143c2f1248a9125
Rolando <84198712+rfs-5017@users.noreply.github.com>
2023-11-23 19:40:47 +0100

    Update index.js

--- final_project/index.js
+++ final_project/index.js
@@ -1,71 +1,37 @@
 const express = require('express');
 const jwt = require('jsonwebtoken');
-let books = require("./booksdb.js");
-const regd_users = express.Router();
+const session = require('express-session')
+const customer_routes = require('./router/auth_users.js').authenticated;
+const genl_routes = require('./router/general.js').general;
 
-let users = [];
+const app = express();
 
-const isValid = (username)=>{ //returns boolean
-//write code to check is the username is valid
-  let userswithsamename = users.filter((user)=>{
-    return user.username === username
-  });
-  if(userswithsamename.length > 0){
-    return true;
-  } else {
-    return false;
-  }
+app.use(express.json());
 
-}
+app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
 
-const authenticatedUser = (username,password)=>{ //returns boolean
-//write code to check if username and password match the one we have in records.
-  let validusers = users.filter((user)=>{
-    return (user.username === username && user.password === password)
-  });
-  if(validusers.length > 0){
-    return true;
-  } else {
-    return false;
-  }
-}
-
-//only registered users can login
-regd_users.post("/login", (req,res) => {
-  //Write your code here
-  const username = req.body.username;
-  const password = req.body.password;
-  if (!username || !password) {
-      return res.status(404).json({message: "Error logging in"});
-  }
-  if (authenticatedUser(username,password)) {
-    let accessToken = jwt.sign({
-      data: password
-    }, 'access', { expiresIn: 60 * 60 });
-
-    req.session.authorization = {
-      accessToken,username
-  }
-  return res.status(200).send("User successfully logged in");
-  } else {
-    return res.status(208).json({message: "Invalid Login. Check username and password"});
-  }
+// * the following code ensures that all the endpoints starting with /customer/auth/ go through the middleware
+app.use("/customer/auth/*", function auth(req,res,next){
+//Write the authenication mechanism here
+    if(req.session.authorization) {
+        token = req.session.authorization['accessToken'];
+        jwt.verify(token, "access",(err,user)=>{
+            if(!err){
+                req.user = user;
+                next();
+            }
+            else{
+                return res.status(403).json({message: "User not authenticated"})
+            }
+        });
+    } else {
+        return res.status(403).json({message: "User not logged in"})
+    }
 });
+ 
+const PORT =5000;
 
-// Add a book review
-regd_users.post("/auth/review/:isbn", (req, res) => {
-  //Write your code here
-  const isbn = req.params.isbn;
-  if (isbn){
-    books[req.body.isbn] = {
-        "author":req.body.author,
-        "title":req.body.title,
-        "review":req.body.review
-        }
-  }
-  res.send("The book" + (' ')+ (req.body.title) + " Has been added!");
-});
+app.use("/customer", customer_routes);
+app.use("/", genl_routes);
 
-module.exports.authenticated = regd_users;
-module.exports.isValid = isValid;
-module.exports.users = users;
+app.listen(PORT,()=>console.log("Server is running"));
